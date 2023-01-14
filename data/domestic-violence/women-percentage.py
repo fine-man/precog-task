bea20425fae4d3f8518d50e21422fe70e2046092
fine-man <spacetime299@gmail.com>
2023-01-14 10:25:19 +0530

    changed women percentage to per 10^6 women

--- data/domestic-violence/women-percentage.py
+++ data/domestic-violence/women-percentage.py
@@ -3,7 +3,7 @@ import pandas as pd
 cases = pd.read_csv("state_total_cases_2010_2018.csv")
 state_pop = pd.read_csv("../processed/state_population_key.csv")
 df = pd.merge(cases, state_pop, how='left', on=['state_name'])
-df['women_percentage'] = df['total_cases']/df['women_population']
+df['women_percentage'] = (df['total_cases'] * (10 ** 6))/df['women_population']
 df = df.sort_values(by=['women_percentage'])
 filename = "state_women_per_2010_2018.csv"
 df[['state_name', 'women_percentage']].to_csv(filename, index=False)
