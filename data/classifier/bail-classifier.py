50a2e09b463d8472b33db087b28ea7ff42b51a15
fine-man <spacetime299@gmail.com>
2023-01-17 23:15:17 +0530

    completed the transform function

--- data/classifier/bail-classifier.py
+++ data/classifier/bail-classifier.py
@@ -31,6 +31,8 @@ disp_filename = "../keys/disp_name_key.csv"
 disp_df = pd.read_csv(disp_filename)
 print(f"loaded file {disp_filename}")
 
+disp_df.drop('count', axis=1, inplace=True)
+
 # strings related to bail judgement
 bail_strings = ['bail granted', 'bail order', 'bail refused', 'bail rejected']
 
@@ -39,17 +41,59 @@ disp_df = disp_df[disp_df['disp_name_s'].isin(bail_strings)]
 
 bail_cases_list = []
 
+# function for data cleaning
 def transform(cases_df):
     # remove unwanted columns
     # change the type of date columns
+    # remove all invalid date columns
+
+    # make 3 new columns for each date column
+    # make a bail column
+    # turn judges into onehotencoding
+    # remove all na values
     
+    cases = cases_df
     # change the type of all date columns
-    date_columns = ['date_of_filing', 'date_of_decision', 'date_first_list', 
-    'date_last_list', 'date_next_list']
+    date_columns = ['date_of_filing', 'date_first_list', 'date_last_list', 
+    'date_next_list', 'date_of_decision']
 
     for column_name in date_columns:
         cases[column_name] = pd.to_datetime(cases[column_name], errors='coerce')
 
+    # make sure the dates are in ascending order
+    for i in range(1, len(date_columns)):
+        column_name = date_columns[i]
+        prev_name = date_columns[i-1]
+        print(column_name, prev_name, end=' ')
+        cases = cases[(cases[column_name] < dt.datetime.now()) &
+                      (cases[column_name] >= cases[prev_name])]
+        print(cases.shape)
+
+    # drop all the NaN values
+    cases.dropna(subset=cases.columns.values)
+    
+    # dropping all useless columns
+    cases.drop(['ddl_case_id', 'cino', 'female_defendant',
+                       'female_petitioner', 'female_adv_def',
+                       'female_adv_pet'], axis=1)
+
+    
+    # adding the bail column
+    cases['bail'] = 0
+    cases.loc[cases['disp_name_s'].isin(bail_strings[0:2]), 'bail'] = 1
+    
+    column_dict = {'date_of_filing' : 'filing', 'date_first_list' : 'first', 
+                   'date_last_list' : 'last', 'date_next_list' : 'next',
+                   'date_of_decision' : 'decision'}
+
+    # transforming all the datetime columns
+    for column_name, name in column_dict.items():
+        cases[f"{name}_year"] = cases[column_name].dt.year
+        cases[f"{name}_month"] = cases[column_name].dt.month
+        cases[f"{name}_day"] = cases[column_name].dt.day
+
+    cases.drop(['judge_position', 'disp_name_s'] + date_columns, axis=1, inplace=True)
+    return cases
 
 def filter_cases(year, disp=disp_df):
     cases = pd.read_csv(f'../cases/cases_{year}.csv')
