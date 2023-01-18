dfa505bf616673d477035ed0b3fa36cec76f7d9f
fine-man <spacetime299@gmail.com>
2023-01-18 20:57:24 +0530

    added pdf.png for new delhi

--- data/new-delhi/best-fit.py
+++ data/new-delhi/best-fit.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import pickle
 import matplotlib.pyplot as plt
 
 # used for plotting random distributions (uses matplotlib underneath)
@@ -16,25 +17,30 @@ dataset = pd.read_csv(filename)
 print("loaded data.csv")
 
 # paramter file
-par_file = open("parameter.txt", "w")
+par_file = "parameter.pkl"
 
+# setting up the plot
 sns.set_style('white')
 sns.set_context('paper', font_scale=2)
 
-sns.displot(data=dataset, x="disposition_days", kind="hist", bins=250, aspect=1.5)
+# plotting the histogram
+sns.displot(data=dataset, x="disposition_days", kind="hist", bins=500, aspect=1.5)
 
+# finding the best fit curve
 disposition_days = dataset["disposition_days"].values
-
 good_distributations = ['betaprime', 'invgamma'] + get_common_distributions()
 
 f = Fitter(disposition_days, timeout=120,
-           distributions=get_common_distributions())
-
-#['invgamma', 'betaprime', 'gamma', 'chi2']
-#['gamma', 'lognorm', 'beta', 'burr', 'norm']
+           distributions=['gamma'])
 
 f.fit()
 print(f.summary())
-print(f.get_best(method='sumsquare_error'))
+parameter_dict = f.get_best(method='sumsquare_error')
 
+# writing the paramters to a file
+with open(par_file, 'wb') as f:
+    pickle.dump(parameter_dict, f)
+
+print(parameter_dict)
+print(f"saved parameters of best fit curve to {par_file}")
 plt.savefig("test.png")
---
+++ data/new-delhi/pdf.py
@@ -0,0 +1,52 @@
+import numpy as np
+from scipy.stats import gamma
+from scipy.stats import expon
+from scipy.stats import invgamma
+import matplotlib.pyplot as plt
+import pandas as pd
+import pickle
+
+fig, ax = plt.subplots()
+
+state_code = 26 # state code for Delhi
+
+filepath = f"days_{state_code}.csv"
+
+# parameters file
+par_file = "parameter.pkl"
+
+save_filepath = "pdf.png"
+
+# loading the dataset for histogram
+df = pd.read_csv(filepath)
+print(f"loaded {filepath}")
+data = list(df["disposition_days"])
+
+# plotting the histogram
+ax.hist(data, bins=500, density=True, ec='black', lw=0.5)
+print("histogram prepared. Finding best fit curve now ...")
+
+# reading the parameter
+with open(par_file, "rb") as f:
+    par_dict = pickle.load(f)
+
+par_dict = par_dict['gamma']
+a = par_dict['a']
+loc = par_dict['loc']
+scale = par_dict['scale']
+
+x = np.linspace(gamma.ppf(0.01, a, loc, scale), gamma.ppf(0.99, a, loc, scale), 500)
+ax.plot(x, gamma.pdf(x, a, loc, scale), label='gamma pdf')
+
+print("Best fit curve prepared")
+ax.legend()
+
+ax.set_xlim(0, 500)
+
+ax.set_xlabel("disposition days")
+ax.set_ylabel("probability density")
+ax.set_title("Best Fit Probability Density Function for Disposition Days of Delhi Cases (2010-2018)")
+ax.grid(True)
+
+plt.show()
+plt.savefig(save_filepath)
