49c2aa8c13fb74fc3a39fd0a41320af46a091e60
fine-man <spacetime299@gmail.com>
2023-01-21 22:50:54 +0530

    made some changes to build.sh

--- data/build.sh
+++ data/build.sh
@@ -1,11 +1,11 @@
 #! /usr/bin/env bash
 
 # make the appropriate directories
-mkdir -p general-analyis/cases_agg general-analysis/temps
+mkdir -p general-analysis/cases_agg general-analysis/temps general-analysis/csv-files
 
-mkdir -p state-analysis/cases_agg state-analysis/temps
+mkdir -p state-analysis/cases_agg state-analysis/temps state-analysis/csv-files
 
-mkdir -p delhi-analysis/cases_agg delhi-analysis/temps
+mkdir -p delhi-analysis/cases_agg delhi-analysis/temps delhi-analysis/csv-files
 
 # unzip the main zip file
 unzip csv.zip
@@ -14,10 +14,10 @@ unzip csv.zip
 tar xvf judges_clean.tar.gz
 
 # extracting the keys folder
-tar xvf keys/keys.tar.gz keys/
+tar xvf keys/keys.tar.gz --directory=keys/
 
 # extract acts and sections file
 tar xvf acts_sections.tar.gz
 
 # extract cases folder
-tar xvf cases/cases.tar.gz cases/
+tar xvf cases/cases.tar.gz --directory=cases/
